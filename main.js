0c52c03859becdecd87c2b64f08bc39761cf5c75
e-lifeCeo <67926478+e-lifeCeo@users.noreply.github.com>
2023-04-06 09:43:55 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,23 @@
-
 const apiButton = document.getElementById('apiButton');
+const apiData = document.getElementById('apiData');
 
-const apiData = document.getElementById('apiData')
+const sats = document.getElementById('sats');
+const texto = document.getElementById('texto');
 
-const sats = document.getElementById('sats')
+const callApi = () =>{
+    fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD').then(res => res.json())
+    .then(data => {
+        apiData.innerText = `Cada BTC equivale a $ ${JSON.stringify(data.BTC.USD)} dolares`
+    sats.innerText = `Cada sat equivale a $ ${JSON.stringify(data.BTC.USD/100000000)} de dolar`
+    var entrada = (document.getElementById('texto').value);
 
-const texto = document.getElementById('texto')
+    var btc = (data.BTC.USD/100000000);
+    var suma = entrada*btc;
+    document.getElementById('resultado').innerHTML = `Total: $ ${suma}`}
+    ,
+       )
+    //    .cacth(e => console.error(new Error(e)));
 
-const callAPI = () =>{
- fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD').then(res => res.json())
-   .then(data => {
-     apiData.innerText = `Cada BTC equivale a $ ${JSON.stringify(data.BTC.USD)
-      } dolares`
-     sats.innerText = `Cada sat equivale a $ ${JSON.stringify(data.BTC.USD/100000000)
-} de dolar`
-var entrada = (document.getElementById('texto').value);
-  
-var btc = (data.BTC.USD/100000000);
-var suma = entrada*btc;
-document.getElementById('resultado').innerHTML = `Total: $ ${suma}`
 }
-   )
-   
-  .catch(e => console.error(new Error(e)));
-    
-}
-/*
-apiButton.addEventListener('click', callAPI);
-*/
 
-
-/*
-setTimeout(function(sats) { window.location=window.location;},1000);*/
-  
-
-setInterval(callAPI, 500);
+setInterval(callApi,1000);
